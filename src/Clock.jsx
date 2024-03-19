5a9b653d053f17b832dcc3ca69932444c637ddf8
nishikantbawa <nishikant.bawankule@powerweave.com>
2024-03-19 20:42:59 +0530

    COnsole log displayed on screen

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Clock = () => {
   // state for current time
   const [time, setTime] = useState(new Date());
+  const [minuteLog, setMinuteLog] = useState([]);
 
   // useEffect hook will runs after rendering of the component
   useEffect(() => {
@@ -14,16 +15,18 @@ const Clock = () => {
       setTime(currentTime);
       // This will check the seconds if they are 0 or 00
       if (currentTime.getSeconds() === 0) {
-        console.log('End of minute reached:', currentTime.toLocaleTimeString());
+        const logMessage = 'End of minute reached: ' + currentTime.toLocaleTimeString();
+        console.log(logMessage);
+        setMinuteLog(prevLog => [...prevLog, logMessage]);
       }
     };
     const intervalID = setInterval(updateTime, 1000);
 
-    // this will return cleanup function to clear the interval when the componentgets unmounted
+    // this will return cleanup function to clear the interval when the component gets unmounted
     return () => {
       clearInterval(intervalID);
     };
-  }, []); 
+  }, []);
 
   function formatTime(time) {
     const hours = time.getHours().toString().padStart(2, '0');
@@ -34,10 +37,19 @@ const Clock = () => {
 
   // Formating the time to display in HH:MM:SS format
   const formattedTime = formatTime(time);
+
   return (
     <div>
       <h2>Digital Clock</h2>
       <p>{formattedTime}</p>
+      <div>
+        <h3>Minute Log</h3>
+        <ul>
+          {minuteLog.map((log, index) => (
+            <li key={index}>{log}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
